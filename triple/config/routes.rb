3f1e66bc51e5805e3d53b2296d2ebd5f5dd4faad
Ken-B <ken.bastiaensen@gmail.com>
2015-02-09 01:09:41 +0100

    added root to routes.rb file

--- triple/config/routes.rb
+++ triple/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'triples/index'
-
   get 'triples/new'
+  root 'triples#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
